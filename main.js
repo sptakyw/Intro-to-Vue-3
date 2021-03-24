177262fa56c630b5d5ed616907cc400ff170c092
sptakyw <6206021620141@fitm.kmutnb.ac.th>
2021-03-24 16:23:18 +0700

    up

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks' ,
+            image: './assets/images/socks_blue.jpg' ,
+            // solution
+            url: 'http://www.vuemastery.com/'
+            // solution
         }
     }
 })
